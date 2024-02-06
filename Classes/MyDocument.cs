e0c14d4a8883d2f0723d7524ce3be368b1e47dbf
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-06 13:58:16 +0200

    new model created

---
+++ Classes/MyDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeacherPlanner.Classes.Interfaces;
+
+namespace TeacherPlanner.Classes
+{
+    internal class MyDocument : ConvertToPDF
+    {
+        public void ConvertToPDF(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
