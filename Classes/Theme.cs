97b0fe8fffad7c79bdb74589f85dce3d2c701e85
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-06 14:02:40 +0200

    theme has lessons

--- Classes/Theme.cs
+++ Classes/Theme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace TeacherPlanner.Classes
 {
     public class Theme
     {
+        public ObservableCollection<Lesson> Lessons { get; set; } = [];
     }
 }
