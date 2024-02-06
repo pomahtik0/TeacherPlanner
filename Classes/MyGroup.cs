5396df4c4edd997d35d862c2468be56bb4e3dbb6
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-06 14:24:18 +0200

    some basic fields added

--- Classes/MyGroup.cs
+++ Classes/MyGroup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,27 @@ namespace TeacherPlanner.Classes
 {
     internal class MyGroup : ConvertToPDF
     {
+        public string GroupName { get; set; } = "";
+        public ObservableCollection<Theme> Themes { get; set; } = [];
+        public int NumberOfThemes
+        {
+            get => Themes.Count;
+            set
+            {
+                if (value < NumberOfThemes)
+                {
+                    Themes = new ObservableCollection<Theme>(Themes.Take(value));
+                }
+                else
+                {
+                    while (value++ > NumberOfThemes)
+                    {
+                        Themes.Add(new Theme());
+                    }
+                }
+            }
+        }
+
         public void ConvertToPDF(string filename)
         {
             throw new NotImplementedException();
