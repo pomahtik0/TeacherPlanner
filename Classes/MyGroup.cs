96a12958e7b4c8761780a19e8e59cd0586ea87d4
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-08 18:16:38 +0200

    field for theme controll

--- Classes/MyGroup.cs
+++ Classes/MyGroup.cs
@@ -49,6 +49,8 @@ namespace TeacherPlanner.Classes
 
         public bool[] daysOfStudy { get; } = new bool[6]; // Mon. Tue. Wed. Thu. Fri. Sat.
 
+        public bool IsThemeControll { get; set; }
+
         public void UploadListOfStudentNames_exel(string fileName)
         {
             // checks for file format
