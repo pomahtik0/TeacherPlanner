f27621959fccbfccbebe1fd7f9ddd1437f18ea73
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-09 00:18:23 +0200

    installing packege to work with excel

--- Classes/MyGroup.cs
+++ Classes/MyGroup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using Microsoft.Office.Interop.Excel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
