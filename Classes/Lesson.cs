34d16a079704fd026877b853e94b6f771e18c0b6
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-09 10:11:35 +0200

    lesson date is now dateTime

--- Classes/Lesson.cs
+++ Classes/Lesson.cs
@@ -9,6 +9,6 @@ namespace TeacherPlanner.Classes
     public class Lesson
     {
         public string Name { get; set; } = "";
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
     }
 }
