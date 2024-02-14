2690dbec40143a97509c86fd99f5aa4c8785d6ad
Roman Havriluk <pomahtik_inc@outlook.com>
2024-02-14 13:03:47 +0200

    some pdf settings class

---
+++ Classes/PdfSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherPlanner.Classes
+{
+    internal class PdfSettings
+    {
+        private string fullPath = string.Empty;
+
+        public string FullPath
+        {
+            get { return fullPath; }
+            set { fullPath = value; }
+        }
+
+        public string DocumentName
+        {
+            get { return Path.GetFileName(fullPath); }
+        }
+
+        public int TextSize { get; set; }
+    }
+}
